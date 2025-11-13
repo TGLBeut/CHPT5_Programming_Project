2acfa8b3e4f25352e9a381d3faa0f787374a6948
TGLBeut <Sgre9381@gmail.com>
2025-11-12 22:06:04 -0500

    .

--- CHPT5_Programming_Project.cpp
+++ CHPT5_Programming_Project.cpp
@@ -1,55 +1,50 @@
 /* File: Ch5_Programming_Project.cpp
    Programmer: Sean G
-   Program 2: Student Line-Up
+   Program 3: Population Bar Chart
    Requirements:
 
-   Read data from an input file until end-of-file (EOF).
-   Apply string comparisons to determine alphabetical order.
-   Output correct information (class size, first student, last student).
+   Open and validate external data files.
+   Use loops to read sequential data and associate values with years.
+   Represent numeric values visually using text-based bar charts.
 */
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iomanip>
-#include <cstdlib>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-	ifstream inputFile;
-	string Name;
-	string FirstStudent;
-	string LastStudent;
+	const int Max_Years = 10;
+	string Years[Max_Years];
+	int Populations[Max_Years];
+	int YearCount = 0;
 
-	int StudentCount = 0;
+	ifstream inputFile("population_data.txt");
 
-	inputFile.open("students.txt");
 	if (!inputFile) {
-		cout << "Error opening the file." << endl;
+		cout << "Error opening file.\n";
 		return 1;
 	}
-	while (inputFile >> Name) {
-		StudentCount++;
 
-		if (StudentCount == 1 || Name < FirstStudent) {
-			FirstStudent = Name;
-		}
-		if (StudentCount == 1 || Name > LastStudent) {
-			LastStudent = Name;
-		}
+	while (YearCount < Max_Years && inputFile >> Years[YearCount] >> Populations[YearCount]) {
+		YearCount++;
 	}
 	inputFile.close();
 
-	cout << "Class Size: " << StudentCount << endl;
-	if (StudentCount > 0)
-	{
-		cout << "First Student: " << FirstStudent << endl;
-		cout << "Last Student: " << LastStudent << endl;
-	}
-	else {
-		cout << "No students found." << endl;
+	cout << "Each * represents 1,000 people\n\n";
+	cout << "Population Bar Chart\n";
+	cout << "---------------------\n";
+	for (int i = 0; i < YearCount; i++) {
+		cout << Years[i] << " | ";
+
+		int BarLength = Populations[i] / 1000;
+
+		for (int b = 0; b < BarLength; b++) {
+			cout << "*";
+		}
+		cout << " (" << Populations[i] << ")" << endl;
 	}
 	return 0;
 }
