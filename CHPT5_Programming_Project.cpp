ed20894c5e1af991640d7398f249acbb3d8b13b4
TGLBeut <Sgre9381@gmail.com>
2025-11-12 22:04:48 -0500

    .

--- CHPT5_Programming_Project.cpp
+++ CHPT5_Programming_Project.cpp
@@ -1,58 +1,55 @@
 /* File: Ch5_Programming_Project.cpp
    Programmer: Sean G
-   Program 1: Population
+   Program 2: Student Line-Up
    Requirements:
 
-   Use loops to model repeated population growth across multiple days.
-   Apply arithmetic calculations involving percentages.
-   Implement input validation for starting size, daily increase, and days.
+   Read data from an input file until end-of-file (EOF).
+   Apply string comparisons to determine alphabetical order.
+   Output correct information (class size, first student, last student).
 */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <iomanip>
+#include <cstdlib>
 #include <cmath>
-
 using namespace std;
 
 int main()
 {
+	ifstream inputFile;
+	string Name;
+	string FirstStudent;
+	string LastStudent;
 
-	int SP; //starting population
-	double DIP; //daily increase percentage
-	int NOD; //number of days to simulate
+	int StudentCount = 0;
 
-	do {
-		cout << "Please enter the starting population (2 or more): ";
-		cin >> SP;
-		if (SP < 2) {
-			cout << "The starting population must be at least 2. Please try again.\n";
+	inputFile.open("students.txt");
+	if (!inputFile) {
+		cout << "Error opening the file." << endl;
+		return 1;
+	}
+	while (inputFile >> Name) {
+		StudentCount++;
+
+		if (StudentCount == 1 || Name < FirstStudent) {
+			FirstStudent = Name;
 		}
-	} while (SP < 2);
-
-	do {
-		cout << "Please enter a positive number for the daily increase percentage: ";
-		cin >> DIP;
-		if (DIP < 0) {
-			cout << "The daily increase percentage cannot be a negative number. Please try again.\n";
+		if (StudentCount == 1 || Name > LastStudent) {
+			LastStudent = Name;
 		}
-	} while (DIP < 0);
+	}
+	inputFile.close();
 
-	do {
-		cout << "Please enter the number of days to simulate (min 1): ";
-		cin >> NOD;
-		if (NOD < 1) {
-			cout << "Number of days must be at least 1. Please try again.\n";
-		}
-	} while (NOD < 1);
-
-	cout << fixed << setprecision(2);
-	cout << "\nDay \tPopulation\n";
-	cout << "---------------------\n";
-
-	for (int day = 1; day <= NOD; ++day) {
-		cout << day << "\t" << SP << endl;
-
-		SP += static_cast <int>(SP * (DIP / 100));
+	cout << "Class Size: " << StudentCount << endl;
+	if (StudentCount > 0)
+	{
+		cout << "First Student: " << FirstStudent << endl;
+		cout << "Last Student: " << LastStudent << endl;
+	}
+	else {
+		cout << "No students found." << endl;
 	}
 	return 0;
 }
